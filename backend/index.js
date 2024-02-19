2b6fded4eaf057dcbfcecfb03176e599b5fa9b16
Jeewanthaherath <jeewanthaherath98@gmail.com>
2024-02-19 15:32:45 +0530

    get all books and get book by id implemented

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,35 @@ app.post('/books' , async (request , response) => {
     }
 })
 
+app.get('/books' , async (request , response) => {
+    try {
+        const books = await Book.find({})
+        return response.status(201).json({
+            count : books.length,
+            data : books
+        })
+        
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({message: error.message})
+        
+    }
+})
+
+app.get('/books/:id' ,  async (request , response) => {
+    try {
+
+        const { id } = request.params
+
+        const book = await Book.findById(id)
+        return response.status(201).json(book)
+        
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({message : error.message})
+    }
+});
+
 
 
 mongoose
